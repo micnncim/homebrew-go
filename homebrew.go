01de8e5dcf90d9fa35934bb05a894fdeff274b78
micnncim <micnncim@gmail.com>
2019-08-23 00:48:29 +0900

    Add comment to homebrew

--- homebrew.go
+++ homebrew.go
@@ -14,10 +14,15 @@ const (
 	searchCmd    = "search"
 )
 
+// Homebrew is a client to run Homebrew command.
 type Homebrew interface {
+	// Install executes `brew install [options] formula`.
 	Install(ctx context.Context, formula string, opts ...InstallOption) error
+	// Uninstall executes `brew uninstall, rm, remove [options] formula`.
 	Uninstall(ctx context.Context, formula string, opts ...UninstallOption) error
+	// List executes `brew list, ls [options] [formula]`.
 	List(ctx context.Context, opts ...ListOption) error
+	// Search executes `brew search [options] [text|/text/]`.
 	Search(ctx context.Context, text string, opts ...SearchOption) error
 }
 
@@ -30,18 +35,21 @@ var _ Homebrew = (*homebrew)(nil)
 
 type Option func(*homebrew)
 
+// WithStdout applied a custom io.Writer as stdout to Homebrew.
 func WithStdout(stdout io.Writer) Option {
 	return func(h *homebrew) {
 		h.stdout = stdout
 	}
 }
 
+// WithStderr applied a custom io.Writer as stderr to Homebrew.
 func WithStderr(stderr io.Writer) Option {
 	return func(h *homebrew) {
 		h.stderr = stderr
 	}
 }
 
+// New instantiates Homebrew with some options.
 func New(opts ...Option) Homebrew {
 	h := &homebrew{
 		stdout: os.Stdout,
